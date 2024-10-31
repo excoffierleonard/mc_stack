6b9aa4c0ffaa76a04938167f00659de956a96b14
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-10-30 22:15:43 -0400

    feat: uniformize error creation

--- scripts/delete_stack.sh
+++ scripts/delete_stack.sh
@@ -4,17 +4,22 @@ format_json() {
     printf '{"message":"%s"}\n' "$1"
 }
 
-[ -z "$1" ] && format_json "Usage: $0 <stack_id>" >&2 && exit 1
+format_error() {
+    echo "{\"message\": \"$1\", \"timestamp\": \"$(date -u +"%Y-%m-%dT%H:%M:%SZ")\"}" >&2
+    exit 1
+}
+
+[ -z "$1" ] && format_error "Usage: $0 <stack_id>"
 
 stack_id=$1
 stack_compose_file="$(dirname "$(realpath "$0")")/../stacks/stack_$stack_id/compose.yaml"
 
-[ ! -f "$stack_compose_file" ] && format_json "Stack $stack_id does not exist" >&2 && exit 1
+[ ! -f "$stack_compose_file" ] && format_error "Stack $stack_id does not exist"
 
-docker compose -f "$stack_compose_file" down > /dev/null 2>&1 || { format_json "Failed to stop stack $stack_id" >&2; exit 1; }
+docker compose -f "$stack_compose_file" down > /dev/null 2>&1 || format_error "Failed to stop stack $stack_id"
 
-docker volume rm "minecraft_server_${stack_id}" > /dev/null 2>&1 || { format_json "Failed to remove minecraft server volume" >&2; exit 1; }
+docker volume rm "minecraft_server_${stack_id}" > /dev/null 2>&1 || format_error "Failed to remove minecraft server volume"
 
-rm -rf "$(dirname "$stack_compose_file")" > /dev/null 2>&1 || { format_json "Failed to remove stack directory" >&2; exit 1; }
+rm -rf "$(dirname "$stack_compose_file")" > /dev/null 2>&1 || format_error "Failed to remove stack directory"
 
 format_json "Stack $stack_id has been successfully deleted"
--- scripts/start_stack.sh
+++ scripts/start_stack.sh
@@ -4,13 +4,18 @@ format_json() {
     printf '{"message":"%s"}\n' "$1"
 }
 
-[ -z "$1" ] && format_json "Usage: $0 <stack_id>" >&2 && exit 1
+format_error() {
+    echo "{\"message\": \"$1\", \"timestamp\": \"$(date -u +"%Y-%m-%dT%H:%M:%SZ")\"}" >&2
+    exit 1
+}
+
+[ -z "$1" ] && format_error "Usage: $0 <stack_id>"
 
 stack_id=$1
 stack_compose_file="$(dirname "$(realpath "$0")")/../stacks/stack_$stack_id/compose.yaml"
 
-[ ! -f "$stack_compose_file" ] && format_json "Stack $stack_id does not exist" >&2 && exit 1
+[ ! -f "$stack_compose_file" ] && format_error "Stack $stack_id does not exist"
 
-docker compose -f "$stack_compose_file" up -d > /dev/null 2>&1 || { format_json "Failed to start stack $stack_id" >&2; exit 1; }
+docker compose -f "$stack_compose_file" up -d > /dev/null 2>&1 || format_error "Failed to start stack $stack_id"
 
 format_json "Stack $stack_id has been successfully started"
